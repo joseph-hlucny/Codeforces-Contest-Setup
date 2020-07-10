792b84b0c8bf3fdf33068b7f768f96c48b481176
Joseph Hlucny <57029780+joseph-hlucny@users.noreply.github.com>
2020-07-10 13:10:36 -0500

    Added cloning of Codeforces-Contest base folder and added creation of subfolders for each problem in the contest.

--- ccs.pl
+++ ccs.pl
@@ -24,12 +24,19 @@ else {
     my $message = $response->decoded_content;
     my $contest_data = from_json($message);
     
-    `git clone --single-branch --branch C++-Language https://github.com/joseph-hlucny/Codeforces-Contest.git`;
+    # Clones base contest folder from Codeforces-Contest git repo
+    my $contest_id = $contest_data->{'result'}->{'contest'}->{'id'};
+    `git clone --single-branch --branch C++-Language https://github.com/joseph-hlucny/Codeforces-Contest.git contest\\Codeforces-Contest`;
+    `ren contest\\Codeforces-Contest $contest_id`;
 
+    # creates a problem subfolder for each problem
     my $problems = $contest_data->{'result'}->{'problems'};
-
     foreach my $element (@$problems) {
-        print $element->{'index'}, "\n";
+        print 'Creating problem ', $element->{'index'}, "...\n";
+        `mkdir contest\\$contest_id\\$element->{'index'}`;
+        `robocopy -s contest\\$contest_id\\sample_problem contest\\$contest_id\\$element->{'index'}`;
+        `ren contest\\$contest_id\\$element->{'index'}\\sample_problem.cpp $element->{'index'}.cpp`;
     }
-	
+    `rmdir /q /s contest\\$contest_id\\sample_problem`;
+    print "Processing complete.\n\nglhf...\n\n";
 }
